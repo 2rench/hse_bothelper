f801d349e67c915dbb0344dd5306a366524db9c3
Витя Пименов <pimenovviktor11@gmail.com>
2026-06-23 20:10:53 +0500

    fix 2.5.8

--- app/bot/handlers/home.py
+++ app/bot/handlers/home.py
@@ -64,6 +64,6 @@ async def home_handler(
     )
 
     await message.answer(
-        "Выбери действие 👇",
+        "",
         reply_markup=get_main_menu(),
     )
--- app/themes/theme_loader.py
+++ app/themes/theme_loader.py
@@ -1,23 +1,21 @@
 from app.themes import (
     default,
-    niche_girl,
     luxury,
     clean_girl,
     brother,
     it_style,
-    # english,
-    # chinese,
-    # french,
+    english,
+    chinese,
+    french,
 )
 
 THEMES = {
     "standard": default,
-    "niche_girl": niche_girl,
     "luxury": luxury,
     "clean_girl": clean_girl,
     "brother": brother,
     "it_style": it_style,
-    # "english": english,
-    # "chinese": chinese,
-    # "french": french,
+    "english": english,
+    "chinese": chinese,
+    "french": french,
 }
